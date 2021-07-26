c0ed1541d4dbc9aca5ac748ab93e1b59bc4f29d3
hyezu <20n2155@u.seijo.ac.jp>
2021-07-26 08:41:42 +0000

    first commit

---
+++ kadai-introduce.rb
@@ -0,0 +1,22 @@
+#コメントアウトされたソースコードはいくら書いても実行されない
+#puts namae='慧珠'
+#puts myouji='白坂'
+#puts nenrei='２１'
+#puts str='白坂慧珠'
+
+myouji='白坂'
+puts myouji
+
+namae='慧珠'
+puts namae
+
+nenrei='21'
+puts nenrei
+
+str1='白坂'
+str2='慧珠'
+str=str1+str2
+puts str
+
+puts str+"です。"
+puts nenrei+"歳です。"

cd916fae5a7b95cecd6f3f5d634295096572db39
hyezu <20n2155@u.seijo.ac.jp>
2021-07-26 09:08:32 +0000

    third commit

--- kadai-introduce.rb
+++ kadai-introduce.rb
@@ -2,15 +2,11 @@
 #puts namae='慧珠'
 #puts myouji='白坂'
 #puts nenrei='２１'
-#puts str='白坂慧珠'
 
 myouji='白坂'
-puts myouji
 
 namae='慧珠'
-puts namae
 
 nenrei='21'
-puts nenrei
 
 puts myouji+namae+"です。"+nenrei+"歳です。"
